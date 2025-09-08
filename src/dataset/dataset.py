a15178dd55f8cd7c884d3d4ddc648afe73714227
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2025-09-08 23:31:20 +0200

    Update dataset.py

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -2,8 +2,7 @@ import os
 import torch
 import h5py
 from torch.utils.data import Dataset, DataLoader
-
-from src.dataset.utils import get_all_files
+from torch.nn.utils.rnn import pad_sequence
 
 
 class H5pyDataReader:
@@ -69,7 +68,7 @@ class H5pyDataset(Dataset):
         Expects config to be a dict-like object containing 'data_dir'.
         Instead of loading all data into memory, only indexes are stored and data is loaded at runtime.
         """
-        
+        from .utils import get_all_files
         self.logger = logger
         self.data_dir = config.get('dataset', {}).get('data_folder', None)
         if not self.data_dir:
@@ -107,7 +106,7 @@ class H5pyDataset(Dataset):
         if idx < 0 or idx >= self.length:
             raise IndexError("Index out of range.")
         file_path, trial_key = self.index_map[idx]
-        
+
         with h5py.File(file_path, 'r') as f:
             g = f[trial_key]
             neural_features = torch.tensor(g['input_features'][:], dtype=torch.float32)
@@ -146,3 +145,5 @@ class DatasetLoader:
         dataloader = DataLoader(dataset, batch_size, shuffle=True, collate_fn=collate_fn)
         self.logger.info(f"Created DataLoader for {kind} with batch size {batch_size}")
         return dataloader
+
+
