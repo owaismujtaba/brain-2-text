766ae17d643205523b537f1178f16bd2eb46a6e3
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2025-09-16 14:05:41 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,25 @@
 import os
 
-from utils import load_yaml_config
+from src.utils import load_yaml_config, log_info
 from src.logging.log import setup_logger
 import pdb
-import torch
 
 if __name__ == "__main__":
     config = load_yaml_config('config.yaml')
-    print("Configuration Loaded:")
-    for key, value in config.items():
-        print(f"{key}: {value}")
-
     logger = setup_logger(
             name=config.get('logging')['name'] , 
             log_file_path=config.get('logging')['log_file_path'], 
             level=config.get('logging')['level'],
             format=config.get('logging')['format']
         )
+    
+    log_info(logger, "Configuration Loaded:")
+    for key, value in config.items():
+        if key=='run':
+            logger.info(f"{key}: {value}")
+
+    
     if config.get('run')['mode'] == 'train':
-        logger.info("Starting training process...")
         from src.dataset.dataset import DatasetLoader
         from src.training.trainer import Trainer
         from src.training.model import BrainToTextModel
@@ -26,14 +27,26 @@ if __name__ == "__main__":
         val_loader = DatasetLoader(config, logger).get_dataloader(kind='val')
 
         model = BrainToTextModel(config)
-        logger.info(model)
         trainer = Trainer(model, config, logger)
         trainer.train(train_loader, val_loader)
 
 
     if config.get('run')['mode'] == 'inference':
-        logger.info('Starting Inference')
+        log_info(logger,'Starting Inference')
         from src.dataset.dataset import DatasetLoader
+        from src.inference.inference import Inference
+        pdb.set_trace()
         test_loader = DatasetLoader(config, logger).get_dataloader(kind='test')
+
+        inferencer = Inference(
+            config=config,
+            logger=logger
+        )
+
+        logits, seq_class_ids, transcripts = inferencer.inference(
+            dataloader=test_loader
+        )
+
+
 
         
